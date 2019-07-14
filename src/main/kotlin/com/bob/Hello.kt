1599c74a256f196b27ee1d705e23fb029a7ed651
Bob <yourspeciman@gmail.com>
2019-07-14 13:07:03 +0800

    Java and Kotlin variable,data type

--- src/main/kotlin/com/bob/Hello.kt
+++ src/main/kotlin/com/bob/Hello.kt
@@ -5,10 +5,15 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age = 25
+    age = age.plus(1)
+    var weight = 66.5f
+    var enroll: Boolean = true
+    println(age)
 }
 
-class Human{
-    fun hello(){
+class Human {
+    fun hello() {
         println("Hello kotlin")
     }
 }
