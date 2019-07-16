de01a9eec2def4a05162490a9bdf8e5a6092b67f
Bob <yourspeciman@gmail.com>
2019-07-16 15:44:44 +0800

    Java and Kotlin guess number

--- src/main/java/com/bob/Hello.java
+++ src/main/java/com/bob/Hello.java
@@ -3,9 +3,21 @@ package com.bob;
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
-        Person p = new Person();
+        String s = "abcde";
+        System.out.println(s.charAt(3));
+        System.out.println(s.length());
+        System.out.println(s.substring(3));
+        System.out.println(s.substring(1,4));
+        Person p = new Person("Bob", 66.5f, 1.7f);
         p.hello();
-        int age = 19;
+        p.weight = 66.5f;
+        p.height = 1.7f;
+        System.out.println(p.bmi());
+        int score = 88;
+        System.out.println(score < 80 || score > 90);
+        char c = 'A';
+        System.out.println(c > 'a');
+       /* int age = 19;
         Integer age2 = 19;
         char c = '我';
         Character c2 = 'A';
@@ -14,6 +26,6 @@ public class Hello {
         double height = 1.7f;
         boolean adult = true;
         boolean enroll = false;
-        String name = "Bob";
+        String name = "Bob";*/
     }
 }
--- src/main/kotlin/com/bob/Hello.kt
+++ src/main/kotlin/com/bob/Hello.kt
@@ -1,18 +1,38 @@
 package com.bob
 
 fun main(args: Array<String>) {
+    var s:String?  = "abcde"
+    s = null
+    println(s?.length)
+    println(s?.get(3))
+    println(s?.substring(3))
 //    println("Hello kotlin")
-//    Human().hello()
-    val h = Human()
-    h.hello()
-    var age = 25
-    age = age.plus(1)
+    val h = Human(weight = 66.5f, height = 1.7f)
+    println(h.bmi())
+    val score = 88
+    println(score > 60)
+    val c: Char = 'A'
+    println(c.toInt() > 60)
+    /*var age = 19
+    age = 20
     var weight = 66.5f
-    var enroll: Boolean = true
-    println(age)
+    var name: String
+    name = "Bob"*/
 }
 
-class Human {
+class Human(var name: String = "", var weight: Float, var height: Float) {
+
+    init {
+        println("test $weight")
+    }
+
+//    constructor(name: String, weight: Float, height: Float):this(weight,height)
+
+    fun bmi(): Float {
+        val bmi = weight / (height * height)
+        return bmi
+    }
+
     fun hello() {
         println("Hello kotlin")
     }
